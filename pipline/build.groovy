eb54b1f003e4839756cd4f3428ad7eb3a295c72f
tzhenguldinov <t.zhenguldinov@softwarecats.dev>
2023-06-21 17:06:46 +0700

    test: set status

---
+++ pipline/build.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    triggers {
+        githubPush()
+    }
+
+    stages {
+
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+
+    }
+
+    post {
+        success {
+            setBuildStatus("Build succeeded", "SUCCESS");
+        }
+        failure {
+            setBuildStatus("Build failed", "FAILURE");
+        }
+    }
+}
---
+++ pipline/vars/setBuildStatus.groovy
@@ -0,0 +1,9 @@
+void setBuildStatus(String message, String state) {
+  step([
+      $class: "GitHubCommitStatusSetter",
+      reposSource: [$class: "ManuallyEnteredRepositorySource", url: "https://github.com/StrongIce/telegram-bot"],
+      contextSource: [$class: "ManuallyEnteredCommitContextSource", context: "${env.JOB_NAME}"],
+      errorHandlers: [[$class: "ChangingBuildStatusErrorHandler", result: "UNSTABLE"]],
+      statusResultSource: [ $class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]] ]
+  ]);
+}
