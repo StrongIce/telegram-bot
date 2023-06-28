2755d928e90702d3983ecde72d4850b85b7e24fd
StrongIce <darkm124@yandex.ru>
2023-06-28 19:48:11 +0700

    update

--- pipline/buildStatus.groovy
+++
@@ -1,9 +0,0 @@
-void call(String message, String state, String repo_url) {
-  step([
-      $class: "GitHubCommitStatusSetter",
-      reposSource: [$class: "ManuallyEnteredRepositorySource", url: "https://github.com/StrongIce/${repo_url}"],
-      contextSource: [$class: "ManuallyEnteredCommitContextSource", context: "${env.JOB_NAME}"],
-      errorHandlers: [[$class: "ChangingBuildStatusErrorHandler", result: "UNSTABLE"]],
-      statusResultSource: [ $class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]] ]
-  ]);
-}
